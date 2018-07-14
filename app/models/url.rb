f04d8abe4e6935606b72da781c5b64e895727d23
cquinones100 <cquinones100@gmail.com>
2018-07-13 22:12:16 -0400

    adds url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,6 +2,7 @@ require './app/models/base_62_converter.rb'
 
 class Url < ApplicationRecord
   validates :original_url, uniqueness: true
+  validates_format_of :original_url, with: /\Ahttps?:\/\/w{3}\.[a-zA-Z0-9\-\_\%\@]+\.[a-zA-Z]+\z?\/?\z?[a-zA-Z\?\=\%0-9\/]+\z/
 
   def self.build_url(url:)
     id = find_last_url_id + 1
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Url, type: :model do
       expect(Url.build_url(url: 'google.com')).to be_an_instance_of(Url)
     end
   end
+
+  describe 'validating format' do
+    it 'rejects an url that does not pass validation' do
+      original_url = 'http://www.hhh'
+
+      url = Url.new(original_url: original_url)
+
+      expect(url.save).to eq(false)
+    end
+
+    it 'allows a url in the right format' do
+      original_url = 'http://www.hhh.com'
+
+      url = Url.new(original_url: original_url)
+
+      expect(url.save).to eq(true)
+    end
+  end
 end
